1279eeeb99d464804b2af379fb74116d6ee14cff
peterlits zo <peterlitszo@gmail.com>
2022-11-06 19:54:06 +0800

    feat: Add support for choices (#1)
    * feat: Add support for choices
    
    Co-authored-by: Peterlits Zo <zhouhongyu.peterlits@bytedance.com>

--- back/db/migrate.sh
+++ back/db/migrate.sh
@@ -1,13 +1,31 @@
 # MAN Document:
 # - 使用 bash migrate.sh 来使用 migration 以将模式迁移到最新模式。
-# - 如果带有参数，那么退化到使用指定了参数的 migrate 命令（见 back/README.md）
+# - 如果带有 "create" 参数，那么将剩下的参数作为名字，生成迁移 SQL 模板。
+# - 如果都不满足，那么退化到使用指定了 database 和 source 参数的 migrate 命令（见 back/README.md）
 
 DIR=$(cd $(dirname $0); pwd)
 
-COMMAND="migrate --database sqlite3://$DIR/database.sqlite --source file://$DIR/migration"
+CREATE_COMMAND="migrate create -ext sql -seq -dir $DIR/migration"
+OTHER_COMMAND="migrate -database sqlite3://$DIR/database.sqlite -source file://$DIR/migration"
 
-if [[ "$1" != "" ]]; then
-    $COMMAND "$@"
+if [[ "$1" == "create" ]]; then
+    shift # 移除 create - 因为 CREATE_COMMDN 已经定义子命令了。
+    $CREATE_COMMAND "$@"
+elif [[ "$1" != "" ]]; then
+    $OTHER_COMMAND "$@"
 else
-    $COMMAND up
+    echo "migrate.sh COMMAND [arg...]"
+    echo
+    echo "migrate.sh 是对 migrate 的简单封装，包括项目相关的命令行配置，有利于保持项目级别的一致。"
+    echo "使用 migrate -help 来了解 migrate 本身的帮助。"
+    echo "如果不带参数运行本脚本，则显示本文档。"
+    echo
+    echo "COMMAND:"
+    echo "  create          调用 migrate create 命令，但是限定了 create 迁移 SQL 路径和命名规则相关参数。"
+    echo "                  建议在后方仅仅添加迁移名，运行之后会在默认的迁移文件夹生成新的空迁移 SQL 文件。"
+    echo "  <others>        其他命令（比如 up，down），会根据默认的迁移文件夹来迁移默认的数据库。"
+    echo
+    echo "默认值:"
+    echo "  默认数据库      <migrate.sh 所在文件夹>/database.sqlite3"
+    echo "  默认迁移文件夹  <migrate.sh 所在文件夹>/migration/"
 fi
