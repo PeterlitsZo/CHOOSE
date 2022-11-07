4bb2fce4bad09132525cef1697bf99b9c11794fd
sunzibin <sunzibin@bytedance.com>
2022-11-07 14:20:49 +0800

    gin+gorm+sqlite

--- back/db/service/model.go
+++ back/db/service/model.go
@@ -2,14 +2,14 @@ package service
 
 // `form:"password" binding:"required"`
 type Answer struct {
-	Id         int64  `gorm:"id;primary_key;AUTO_INCREMENT" json:"id"`
-	UserName   string `gorm:"user_name" json:"user_name"`
-	QuestionId string `gorm:"question_id" json:"question_id"`
-	Answer     string `gorm:"answer" json:"answer"`
+	Id         int64  `gorm:"id;primary_key;AUTO_INCREMENT" json:"id" form:"id"`
+	UserName   string `gorm:"user_name" json:"user_name" form:"user_name"`
+	QuestionId string `gorm:"question_id" json:"question_id" form:"question_id"`
+	Answer     string `gorm:"answer" json:"answer" form:"answer"`
 }
 
 type Question struct {
-	Id       int64  `gorm:"id" json:"id"`
-	Context  string `gorm:"context" json:"context"`
-	Question string `gorm:"question" json:"question"`
+	Id       int64  `gorm:"id" json:"id" form:"id"`
+	Context  string `gorm:"context" json:"context" form:"context"`
+	Question string `gorm:"question" json:"question" form:"question"`
 }
